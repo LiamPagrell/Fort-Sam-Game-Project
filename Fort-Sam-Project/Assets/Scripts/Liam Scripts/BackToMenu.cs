e7e709fc24eff9fbfd54cab6ecdc08e254a11ebc
FeliciaTorwald <113012104+FeliciaTorwald@users.noreply.github.com>
2022-12-14 10:44:34 +0100

    escape

--- Fort-Sam-Project/Assets/Scripts/Liam Scripts/BackToMenu.cs
+++ Fort-Sam-Project/Assets/Scripts/Liam Scripts/BackToMenu.cs
@@ -1,29 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class BackToMenu : MonoBehaviour
 {
-    
+    public GameObject lockPanel;
     // Start is called before the first frame update
     void Start()
     {
 
         
+   
 
     }
 
-    // Update is called once per frame
+     
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
+        
+       
+        
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (lockPanel.activeSelf)
+                {
+                    bool isActive = lockPanel.activeSelf;
+                    lockPanel.SetActive(!isActive);
+                    StartCoroutine(Wait());
 
-            SceneManager.LoadScene("Menu");
-           
+                }
 
-        }
+                else if (!lockPanel.activeSelf)
+                {
+                    SceneManager.LoadScene("Menu");
+
+                }
+            }
+
         
     }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(3f);
+        Debug.Log("Poopy1");
+        
+        
+    }
+
 }
