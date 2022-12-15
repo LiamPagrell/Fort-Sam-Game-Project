e3f2752356e10e3f0baffeb8966446aabde075b4
Cym3ra <112971484+Cym3ra@users.noreply.github.com>
2022-12-15 15:50:59 +0100

    calender script changed

--- Fort-Sam-Project/Assets/Scripts/Felicia Scripts/UnlockCalendar.cs
+++ Fort-Sam-Project/Assets/Scripts/Felicia Scripts/UnlockCalendar.cs
@@ -7,30 +7,31 @@ public class UnlockCalendar : MonoBehaviour
     public GameObject lockPanel;
     public SoundManager soundManager;
 
-    Movement player;
+    //Movement player;
 
-    float checkDistance;
-    float pickUpDistance = 3.6f;
+    //float checkDistance;
+    //float pickUpDistance = 3.6f;
 
 
     private void Start()
     {
-        player = FindObjectOfType<Movement>();
+        //player = FindObjectOfType<Movement>();
     }
 
     private void OnMouseDown()
     {
-        checkDistance = Vector2.Distance(this.transform.position, player.transform.position);
+        //checkDistance = Vector2.Distance(this.transform.position, player.transform.position);
 
-        if (checkDistance < pickUpDistance)
+        if (lockPanel != null)
         {
-            if (lockPanel != null)
-            {
-                bool isActive = lockPanel.activeSelf;
-                lockPanel.SetActive(!isActive);
-                soundManager.TurningPages();
-            }
+            bool isActive = lockPanel.activeSelf;
+            lockPanel.SetActive(!isActive);
+            soundManager.TurningPages();
         }
+        /*if (checkDistance < pickUpDistance)
+        {
+
+        }*/
 
     }
 }
