80cc99726b799c3f859c9c5ed03ad28631c42e6a
Cym3ra <112971484+Cym3ra@users.noreply.github.com>
2022-12-21 11:45:43 +0100

    Pausecontroller script added

---
+++ Fort-Sam-Project/Assets/Scripts/Carina Scripts/PauseController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseController : MonoBehaviour
+{
+
+    [SerializeField] GameObject pausePanel;
+    [SerializeField] GameObject sockText, backpack;
+
+    public static bool gameIsPaused;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            gameIsPaused = !gameIsPaused;
+            PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        if (gameIsPaused)
+        {
+            pausePanel.SetActive(true);
+            Time.timeScale = 0f;        // stops time
+            AudioListener.pause = true; // paus all music
+            sockText.SetActive(false); 
+            backpack.SetActive(false);
+        }
+        else
+        {
+            ResumeGame();
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+        AudioListener.pause = false;
+        sockText.SetActive(true);
+        backpack.SetActive(true);
+    }
+
+    public void QuitToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
