0de319a48f93d7ff21ab6b363a0bff7b07555ee8
LiamPagrell <113012181+LiamPagrell@users.noreply.github.com>
2022-12-23 14:31:47 +0100

    fixed distance for bedncoverscript

--- Fort-Sam-Project/Assets/Scripts/Felicia Scripts/BednCover.cs
+++ Fort-Sam-Project/Assets/Scripts/Felicia Scripts/BednCover.cs
@@ -4,42 +4,39 @@ using UnityEngine;
 
 public class BednCover : MonoBehaviour
 {
-    //public GameObject roboteyes;
-    //public GameObject cat;
-    //public GameObject rat;
-    //public GameObject bed;
-    //public GameObject cat;
     public GameObject remote;
     public GameObject checkmark;
     public GameObject sam;
-    //[SerializeField] AudioSource clickSound;
-    //Movement player;
     public SoundManager soundManager;
+
+    float checkDistance;
+    float pickUpDistance = 4f;
+
+    public void Start()
+    {
+        
+    }
     public void OnMouseDown()
     {
-        //bed.gameObject.GetComponent<Animator>().SetTrigger("Sheet");
-        Debug.Log("Start");
-        sam.gameObject.GetComponent<Animator>().SetTrigger("PickingUpMedium");
-        //GameObject.Find("Sam_Placeholder").GetComponent<Animator>().SetTrigger("PickingUpMedium");
-        Debug.Log("Sam");
-        GameObject.Find("PillowAndBlanket_Placeholder").GetComponent<Animator>().SetTrigger("Sheet");
-        Debug.Log("Bed");
-        GameObject.Find("Cat").GetComponent<Animator>().SetTrigger("CatActive");
-        Debug.Log("Cat");
-        GameObject.Find("BigWhiskers").GetComponent<Animator>().SetTrigger("RatRun");
-        Debug.Log("Rat");
-        //cat.gameObject.GetComponent<Animator>().SetTrigger("CatActive");
-        //rat.gameObject.GetComponent<Animator>().SetTrigger("RatRun");
+        checkDistance = Vector2.Distance(this.transform.position, sam.transform.position);
 
-        soundManager.Cozies();
-        soundManager.Purr();
-        Debug.Log("täcke");
-        checkmark.SetActive(true);
-        Destroy(this.gameObject);
-        //roboteyes.gameObject.GetComponent<Animator>().SetTrigger("CatJump");
-        //cat.gameObject.GetComponent<Animator>().SetTrigger("CatAnim");
-        //rat.gameObject.GetComponent<Animator>().SetTrigger("RatAnim");
-        //StartCoroutine(wait());
+        if (Input.GetMouseButtonDown(0) && checkDistance < pickUpDistance && CompareTag("Interactable"))
+        {
+
+            sam.gameObject.GetComponent<Animator>().SetTrigger("PickingUpMedium");
+            GameObject.Find("PillowAndBlanket_Placeholder").GetComponent<Animator>().SetTrigger("Sheet");
+            GameObject.Find("Cat").GetComponent<Animator>().SetTrigger("CatActive");
+            GameObject.Find("BigWhiskers").GetComponent<Animator>().SetTrigger("RatRun");
+
+
+
+            soundManager.Cozies();
+            soundManager.Purr();
+
+            checkmark.SetActive(true);
+            Destroy(this.gameObject);
+        }
+
 
 
     }
