1ae4220c35a87048a223adb4939cf239e3a5d653
FeliciaTorwald <113012104+FeliciaTorwald@users.noreply.github.com>
2022-12-30 16:37:03 +0100

    dad no more moonwalk no more teleportation

--- Fort-Sam-Project/Assets/Scripts/Felicia Scripts/KitchenLock.cs
+++ Fort-Sam-Project/Assets/Scripts/Felicia Scripts/KitchenLock.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Unity.VisualScripting;
 
 public class KitchenLock : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class KitchenLock : MonoBehaviour
     public GameObject cheese;
     public GameObject locket;
     public GameObject rat;
+    bool foundCode;
 
     void Start()
     {
@@ -19,25 +21,38 @@ public class KitchenLock : MonoBehaviour
    
     void Update()
     {
-        CheckCode();
+       
+        if (!foundCode)
+        {
+            CheckCode();
+        }
+        
     }
     public void CheckCode()
     {
         if (Input.text == "gilbert")
         {
+            foundCode = true;
+            Debug.Log("rightCode");
             StartCoroutine(WinCLosePanel());
         }
     }
+
+    
     IEnumerator WinCLosePanel()
     {
         yield return new WaitForSeconds(0.5f);
-        this.gameObject.SetActive(false);
+        //this.gameObject.SetActive(false);
         locket.gameObject.SetActive(false);
-        fridge.SetActive(true);
+        fridge.gameObject.SetActive(true);
         cheese.gameObject.GetComponent<Animator>().SetTrigger("cheese");
         yield return new WaitForSeconds(1f);
+        Debug.Log("working");
         fridge.gameObject.SetActive(false);
-        //rat.gameObject.GetComponent<Animator>().SetTrigger("rat");
+        rat.gameObject.GetComponent<Animator>().SetTrigger("RatRunnin");
+        rat.gameObject.GetComponent<BoxCollider2D>().enabled= false;
+
+        Input.gameObject.SetActive(false);
 
 
     }
