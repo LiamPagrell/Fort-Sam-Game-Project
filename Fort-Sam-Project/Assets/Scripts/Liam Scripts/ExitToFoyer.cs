6cc9d24283541f3140db220c3f5c23fc52535c99
LiamPagrell <113012181+LiamPagrell@users.noreply.github.com>
2023-01-02 13:52:51 +0100

    Revert "ping61"
    This reverts commit 73e03cb375494c53c123a844830a9f2cfddbbb1e.

--- Fort-Sam-Project/Assets/Scripts/Liam Scripts/ExitToFoyer.cs
+++ Fort-Sam-Project/Assets/Scripts/Liam Scripts/ExitToFoyer.cs
@@ -13,8 +13,8 @@ public class ExitToFoyer : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
-            GameObject.Find("Characters").GetComponent<Animator>().SetTrigger("TransitLiving");
-            GameObject.Find("TransitionSam").GetComponent<Animator>().SetTrigger("TransitionLiving");
+            GameObject.Find("Characters").GetComponent<Animator>().SetTrigger("Transit");
+            GameObject.Find("TransitionSam").GetComponent<Animator>().SetTrigger("Transition");
         }
     }
     // Update is called once per frame
