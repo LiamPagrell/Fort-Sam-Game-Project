d921c8f664e633f44a02fa6d941f2d7c571f5191
Cym3ra <112971484+Cym3ra@users.noreply.github.com>
2023-01-03 21:16:45 +0100

    Hallway door to attic updated

---
+++ Fort-Sam-Project/Assets/Scripts/Carina Scripts/AtticTransition.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtticTransition : MonoBehaviour
+{
+
+    [SerializeField] GameObject goToAtticPopUp;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && goToAtticPopUp.activeSelf)
+        {
+            GoUpStairs();
+        }
+
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.A))
+        {
+            FindObjectOfType<DoorAtticTransition>().MoveFromDoor();
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            goToAtticPopUp.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            goToAtticPopUp.SetActive(false);
+        }
+    }
+
+    private void GoUpStairs()
+    {
+        StartCoroutine(StairsTransition());
+    }
+
+    IEnumerator StairsTransition()
+    {
+        yield return new WaitForSeconds(0.5f);
+        goToAtticPopUp.SetActive(false);
+        FindObjectOfType<DoorAtticTransition>().DoorClosed();
+    }
+}
