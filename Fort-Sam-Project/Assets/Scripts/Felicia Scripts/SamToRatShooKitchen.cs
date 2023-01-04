10cbd19b286c0c207997c630f55062d91c1d8ca4
LiamPagrell <113012181+LiamPagrell@users.noreply.github.com>
2023-01-04 16:03:56 +0100

    ping71

--- Fort-Sam-Project/Assets/Scripts/Felicia Scripts/SamToRatShooKitchen.cs
+++ Fort-Sam-Project/Assets/Scripts/Felicia Scripts/SamToRatShooKitchen.cs
@@ -4,19 +4,35 @@ using UnityEngine;
 
 public class SamToRatShooKitchen : MonoBehaviour
 {
+    Movement player;
+
+    public void Start()
+    {
+        player = FindObjectOfType<Movement>();
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("BigRat"))
         {
+            player.gameObject.GetComponent<Movement>().StopMovement();
+            player.gameObject.GetComponent<Movement>().enabled = false;
             GameObject.Find("BigWhiskers").GetComponent<Animator>().SetTrigger("RatShoo");
             StartCoroutine(Wait());
+            Invoke(nameof(StartMove), 1f);
         }
     }
 
     IEnumerator Wait()
     {
+
         yield return new WaitForSecondsRealtime(0.3f);
         GameObject.Find("Sam_placeholder").GetComponent<Animator>().SetTrigger("ShooReact");
+    }
+
+    public void StartMove()
+    {
+        player.gameObject.GetComponent<Movement>().enabled = true;
+        player.gameObject.GetComponent<Movement>().StartMovement();
     }
 
         
--- Fort-Sam-Project/Assets/TriggerRat.cs
+++ Fort-Sam-Project/Assets/TriggerRat.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 public class TriggerRat : MonoBehaviour
 {
     public GameObject rat;
+    PickupBehaviour pockpick;
     // Start is called before the first frame update
     void Start()
     {
-        
+        pockpick = FindObjectOfType<PickupBehaviour>();
     }
 
     public void OnTriggerEnter2D(Collider2D coleslaw)
     {
-        if(coleslaw.CompareTag("Player"))
+        if (pockpick.GotCheese == true)
         {
-            rat.gameObject.GetComponent<Animator>().SetTrigger("Consume");
-            rat.gameObject.GetComponent<CapsuleCollider2D>().enabled = false;
+            if (coleslaw.CompareTag("Player"))
+            {
+                rat.gameObject.GetComponent<Animator>().SetTrigger("Consume");
+                rat.gameObject.GetComponent<CapsuleCollider2D>().enabled = false;
+            }
+
+        }
+
+        else
+        {
+            return;
         }
     }
 
