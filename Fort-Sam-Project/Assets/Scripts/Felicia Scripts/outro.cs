40b6d59445a3874043e44a6948635686292ff972
FeliciaTorwald <113012104+FeliciaTorwald@users.noreply.github.com>
2023-01-04 20:26:28 +0100

    outro

--- Fort-Sam-Project/Assets/Scripts/Felicia Scripts/outro.cs
+++ Fort-Sam-Project/Assets/Scripts/Felicia Scripts/outro.cs
@@ -5,18 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class outro : MonoBehaviour
 {
+
+    [SerializeField] Animator animator;
+    [SerializeField] GameObject transitionImage;
+    [SerializeField] GameObject transitionText;
+
     // Start is called before the first frame update
     void Start()
     {
+
+        animator.SetTrigger("TriggerTransition");
         LoadEndScene();
+
     }
     public void LoadEndScene()
     {
+
         StartCoroutine(FortFinished());
     }
 
     IEnumerator FortFinished()
     {
+ 
+
+        yield return new WaitForSecondsRealtime(2f);
+   
+        transitionText.SetActive(true);
+        //transitionImage.SetActive(false);
+        yield return new WaitForSecondsRealtime(3f);
+        animator.SetTrigger("FadeIn");
         yield return new WaitForSecondsRealtime(3f);
         SceneManager.LoadScene("Outro2");
 
